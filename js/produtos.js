9a48ccfe476d86fb858bcbad6de1eb02a1c69a34
disclousureshop <disclousure.shop@gmail.com>
2023-02-27 21:46:45 -0300

    Criação do projeto

---
+++ js/produtos.js
@@ -0,0 +1,32 @@
+var prod = '';
+var produtos = {"produto":  [
+    {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular1', "link": 'google.com.br'},
+    {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular2', "link": 'google.com.br'},
+    {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular3', "link": 'google.com.br'},
+    {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular4', "link": 'google.com.br'}
+]};
+
+
+for (let i = 0; i < produtos.produto.length; i++) {
+    prod += `<a class='col-6 mb-3 p-0' target='_blank' href='${produtos.produto[i].link}'><div class='container'><div class='card' width='500px'><img src='./${produtos.produto[i].img}' class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>${produtos.produto[i].nome}</h5></div></div></div></a>`;
+
+}
+document.getElementById("produtos").innerHTML = prod;
+
+
+
+function searchProd() {
+    var input, filter, cards, cardContainer, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("produtos");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-title");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+        cards[i].style.display = "";
+        } else {
+        cards[i].style.display = "none";
+        }
+    }
+}
