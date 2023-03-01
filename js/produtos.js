06baeed143bbb4d10cb7a67ea7e7d7509d8a63c9
disclousureshop <disclousure.shop@gmail.com>
2023-03-01 13:28:13 -0300

    PRODUTOS

--- js/produtos.js
+++ js/produtos.js
@@ -3,14 +3,14 @@
 
 var prod = '';
 var produtos = {"produto":  [
-    {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular1', "link": 'https://google.com.br'},
-    {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular2', "link": 'https://google.com.br'},
+    {"img": 'assets/logo.jpeg', "nome": '01 - S7 SMART WATCH', "link": 'https://shope.ee/7KPi0TLmAj'},
+    {"img": 'assets/logo.jpeg', "nome": '02 - CAIXINHA DE SOM', "link": 'https://shope.ee/1flLMgHhkV'},
     {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular3', "link": 'https://google.com.br'},
     {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular4', "link": 'https://google.com.br'}
 ]};
 
 for (let i = 0; i < produtos.produto.length; i++) {
-    prod += "<div class='col-6 mb-3 p-0'><div class='container'><a class='card' href='"+produtos.produto[i].link+"' width='500px'><img src='./"+produtos.produto[i].img+"' class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>"+produtos.produto[i].nome+"</h5></div></a></div></div>";
+    prod += "<div class='col-6 mb-3 p-0'><div class='container'><a class='card' target='_blank' href='"+produtos.produto[i].link+"' width='500px'><img src='./"+produtos.produto[i].img+"' class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>"+produtos.produto[i].nome+"</h5></div></a></div></div>";
 }
 
 document.getElementById("produtos").innerHTML = prod;
