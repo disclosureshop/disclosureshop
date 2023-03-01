8c29bbe982c9caa4dd27fd608cd7d22a1ed6204e
disclousureshop <disclousure.shop@gmail.com>
2023-03-01 13:12:20 -0300

    teste

--- js/produtos.js
+++ js/produtos.js
@@ -6,13 +6,11 @@ var produtos = {"produto":  [
     {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular4', "link": 'www.google.com.br'}
 ]};
 
-
 for (let i = 0; i < produtos.produto.length; i++) {
-    prod += `<div class='col-6 mb-3 p-0'><div class='container'><a class='card' href='${produtos.produto[i].link}' width='500px'><img src='./${produtos.produto[i].img}' class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>${produtos.produto[i].nome}</h5></div></a></div></div>`;
-
+    prod += "<div class='col-6 mb-3 p-0'><div class='container'><a class='card' href='"+produtos.produto[i].link+"' width='500px'><img src='./"+produtos.produto[i].img+"' class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>"+produtos.produto[i].nome+"</h5></div></a></div></div>";
 }
-document.getElementById("produtos").innerHTML = prod;
 
+document.getElementById("produtos").innerHTML = prod;
 
 function searchProd() {
     var input, filter, cards, cardContainer, title, i;
