b3c879dcfe22e4f1716bf2554bf48efc6a2ea610
disclousureshop <disclousure.shop@gmail.com>
2023-03-01 13:40:31 -0300

    produtos

--- js/produtos.js
+++ js/produtos.js
@@ -5,7 +5,7 @@ var prod = '';
 var produtos = {"produto":  [
     {"img": 'https://cf.shopee.com.br/file/sg-11134201-22100-hp92pecueriv45', "nome": '01 - S7 SMART WATCH', "link": 'https://shope.ee/7KPi0TLmAj'},
     {"img": 'https://cf.shopee.com.br/file/ce5cf94bfd3acbc8bb4c633ad2dbcbb3', "nome": '02 - CAIXINHA DE SOM', "link": 'https://shope.ee/1flLMgHhkV'},
-    {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular3', "link": 'https://google.com.br'},
+    {"img": 'https://cf.shopee.com.br/file/8b6cb757b119f3f3589151890e6fa99b', "nome": 'Capa De Silicone Kuromi Airpods', "link": 'https://shope.ee/8KIFDGt76B'},
     {"img": 'assets/logo.jpeg', "nome": 'Carregador de celular4', "link": 'https://google.com.br'}
 ]};
 
