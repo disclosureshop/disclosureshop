6f3662b226c73a223357a417b39f1c0a91cb6e32
disclousureshop <disclousure.shop@gmail.com>
2023-03-11 22:46:07 -0300

    produtos

--- js/produtos.js
+++ js/produtos.js
@@ -4,13 +4,14 @@
 var prod = '';
 var produtos = {"produto":  [
     // {"img": 'assets/logo.jpeg', "nome": '04 - fone de ouvido sem fio bluetooth Pikachu', "link": ''},
+    {"img": 'https://cf.shopee.com.br/file/e76c1efb6b1ef420763bb3a0e79153c1', "nome": '08 - Placa Ripada 30 x 30CM kit com 22peças 260 reais', "link": 'https://shope.ee/4pidpLGk0S'},
     {"img": 'https://cf.shopee.com.br/file/b21dc08deb5ca92c8b6a7b9c37afb0ce', "nome": '07 - Jogo de Panela Antiaderente de 150 reais', "link": 'https://shope.ee/4KmLAexeNc'},
     {"img": 'https://cf.shopee.com.br/file/cd7ac136a61f6fc9b6086d1f48f77b70', "nome": '06 - Chapéu Pelucia Polvo-mau', "link": 'https://shope.ee/8KIFMyJH2P'},
     {"img": 'https://cf.shopee.com.br/file/sg-11134201-22110-fygvzsdk2hjvcc', "nome": '05 - Placa decorativa 3D PVC', "link": 'https://shope.ee/1Ap6LbohgC'},
     {"img": 'https://cf.shopee.com.br/file/298a2964003347b84448c9d0972f169a', "nome": '04 - Fone De Ouvido Sem Fio Bluetooth Pikachu', "link": 'https://shope.ee/5KedeQWAxl'},
     {"img": 'https://cf.shopee.com.br/file/8b6cb757b119f3f3589151890e6fa99b', "nome": '03 - Fone De Ouvido Com Capa De Silicone Kuromi Airpods', "link": 'https://shope.ee/8KIFDGt76B'},
     {"img": 'https://cf.shopee.com.br/file/ce5cf94bfd3acbc8bb4c633ad2dbcbb3', "nome": '02 - Caixinha De Som', "link": 'https://shope.ee/1flLMgHhkV'},
-    {"img": 'https://cf.shopee.com.br/file/sg-11134201-22100-talzofauerivf4', "nome": '01 - S7 Smart Watch', "link": 'https://shope.ee/7KPi0TLmAj'}
+    {"img": 'https://cf.shopee.com.br/file/sg-11134201-22100-talzofauerivf4', "nome": '01 - S7 Smart Watch', "link": 'https://shope.ee/7KPi0TLmAj'},
 ]};
 
 for (let i = 0; i < produtos.produto.length; i++) {
